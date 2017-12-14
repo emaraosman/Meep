57e6cd6da1bfaed3559d50ea3b833af13732db49
Osman Emara <emaraosman@gmail.com>
2017-12-14 14:48:09 -0500

    setup for react

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
 import './App.css';
+
+import { BrowserRouter as Router, Link, Route, Redirect } from 'react-router-dom';
+import Auth from './modules/Auth';
+import Nav from './components/Nav';
+import Home from './components/Home';
+
 
 class App extends Component {
   render() {
     return (
+      <Router>
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+        <Home />
+
       </div>
+      </Router>
     );
   }
 }
---
+++ client/src/components/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div className="hero">
+      <h1>Welcome to Meep</h1>
+    </div>
+  )
+}
+
+export default Home;
